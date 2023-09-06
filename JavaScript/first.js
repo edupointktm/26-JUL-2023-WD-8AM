a8b7056a360a96277f22820c70040d19e50b2fe9
edupointktm <edupointktm@gmail.com>
2023-09-06 09:32:41 +0545

    Javascript-first

---
+++ JavaScript/first.js
@@ -0,0 +1,14 @@
+// document.write("<h1> My Name is Prabhat </h1>")
+// console.log(" My Name is prabhat ")
+
+
+// var names = "Prabhat"
+
+// document.write(`<h1> ${names} </h1>`)
+// document.write("<h1>"" + names + " </h1>")
+// document.write("Kathamndu")
+
+
+var names = "Broadway Infosys"
+
+console.log(names)
