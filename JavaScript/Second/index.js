6f52c44155b11f3d673bf7ed8d4d2f6dd32fa2b4
edupointktm <edupointktm@gmail.com>
2023-09-10 09:01:16 +0545

    operator

---
+++ JavaScript/Second/index.js
@@ -0,0 +1,31 @@
+// var names = prompt("Enter you name")
+// document.write("My name is " + names)
+// console.log("My name is " + names)
+
+// var first_num = prompt("Enter first number")
+// var second_num = prompt("Enter second number ")
+// var sum = parseInt(first_num) + parseInt(second_num)
+// document.write("Sum = " + sum)
+// console.log("Sum = " +sum)
+// alert("Sum = "+sum)
+
+// a="Prabhaat"
+// console.log(" My Name is "+a)
+// // document.write( "My Name is "+a)
+
+// Declare and Assign
+
+// var a = "Ram"
+// var a = "Mohan"
+// console.log(a)
+
+// it doesn't re declare but re-assign 
+// let a="Ram"
+//  a = "Mohan"
+// console.log(a)
+
+// No re-decalre and no value re-assign
+const a= "Ram"
+const b = "Mohan"
+
+console.log(a)
---
+++ JavaScript/operator/index.js
@@ -0,0 +1,34 @@
+
+var a=5
+var b=2
+
+// console.log(a+b)  // 12
+// console.log(a-b)  // -2
+// console.log(a*b)  // 35
+// console.log(a/b)  // 0.00
+// console.log(a%b)  // 0.00
+// console.log(a)  // 0.00
+// console.log(++a)  // 0.00
+// console.log(a)  // 0.00
+// console.log(--a)  // 0.00
+// console.log(a**b)
+
+// var a=5
+// b=a;
+// console.log(b+=1) // b= b+1
+// console.log(b-=1) // b= b-1
+
+//Conditional  or comparision Operator are : < <= > >= == === !=
+//it return the result as => True or Flase
+
+// console.log("4"===4)  //
+
+
+// Logic Operator AND (&&) OR (||) Not (!) : it combine comparision 
+//it return the result as => ture or flase
+//AND = if all conditions are ture then result will be True, if any one conditon is flase then result will be false.
+//OR = if any one condtion is true then result will be true.
+// console.log(6>2 || 7>3 || 9>4) //=> Result =False
+// console.log(6>2 && 7>3 && 9>4) //=> Result =False
+// console.log(true && true && true ) //=> Result =False
+// console.log(true || flase || true ) //=> Result =False
