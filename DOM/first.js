d01944a96be4f87373e5986ee6cc84a676b3b57d
edupointktm <edupointktm@gmail.com>
2023-09-15 09:34:24 +0545

    dom

---
+++ DOM/first.js
@@ -0,0 +1,2 @@
+// console.log(document.querySelectorAll("h1")[0].innerText)
+// document.querySelector("#box").style.color="red"
---
+++ JavaScript/questions2.js
@@ -0,0 +1,6 @@
+for (var i = 1; i <= 5; i++) {
+    for (var j = 5; j >= i; j--) {
+        document.write(j);
+    }
+    document.write("<br>")
+}
