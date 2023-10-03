4456bdefadaf8477123247529eb99abd20e36d70
edupointktm <edupointktm@gmail.com>
2023-10-03 09:39:32 +0545

    useeffect

--- React/useEffect/use_effect/src/Example_of_use_effect.jsx
+++ React/useEffect/use_effect/src/Example_of_use_effect.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Example_of_use_effect() {
-  return (
-    <div>Example_of_use_effect</div>
-  )
+let [count, updateCount]=useState(5)
+let [con, updateCon]=useState(10)
+
+useEffect(()=>{
+    console.log("I am work on effect "+count + con)
+},[con])
+
+    return (
+        <>
+            <h1>
+                <button onClick={()=>updateCount(count+1)}>+</button>
+                <span> {count} </span>
+                <button onClick={()=>updateCount(count-1)}> - </button>
+            </h1>
+            <hr/>
+            <h1>
+                <button onClick={()=>updateCon(con+1)}>+</button>
+                <span> {con} </span>
+                <button onClick={()=>updateCon(con-1)}> - </button>
+            </h1>
+        </>
+    )
 }
 
 export default Example_of_use_effect
